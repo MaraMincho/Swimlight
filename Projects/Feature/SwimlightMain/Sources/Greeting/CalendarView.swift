c8b7661c2ba1517a3df66ee539edbb29a6d0d6a7
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2024-09-16 10:40:26 +0900

    feat: dateRange조정

--- Projects/Feature/SwimlightMain/Sources/Greeting/CalendarView.swift
+++ Projects/Feature/SwimlightMain/Sources/Greeting/CalendarView.swift
@@ -22,13 +22,41 @@ final class SLCalendarViewController: UIViewController {
     calendarView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
   }
 
-  var calendarView: UICalendarView = {
+  let gregorianCalendar = Calendar(identifier: .gregorian)
+  lazy var calendarView: UICalendarView = {
     let view = UICalendarView()
     view.tintColor = UIColor(SLColor.main01.color)
+
+    view.calendar = gregorianCalendar
+    view.fontDesign = .rounded
+
+    if let fromDate = fromDateComponents().date,
+       let toDate = makeTodayDateComponent().date {
+      let calendarViewDateRange = DateInterval(start: fromDate, end: toDate)
+      view.availableDateRange = calendarViewDateRange
+    }
 
     view.translatesAutoresizingMaskIntoConstraints = false
     return view
   }()
+
+  private func fromDateComponents() -> DateComponents {
+    DateComponents(calendar: gregorianCalendar, year: 2024, month: 1, day: 1)
+  }
+
+  private func makeTodayDateComponent() -> DateComponents {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd"
+    let dateString = dateFormatter.string(from: .now)
+    let components = dateString.split(separator: "-").map { Int($0) ?? 0 }
+    
+    return DateComponents(
+      calendar: gregorianCalendar,
+      year: components[safe: 0],
+      month: components[safe: 1],
+      day: components[safe: 2]
+    )
+  }
 }
 
 // MARK: - SLCalendarView
@@ -54,4 +82,14 @@ struct SLCalendarView: UIViewControllerRepresentable {
   func updateUIViewController(_: SLCalendarViewController, context _: Context) {}
 
   typealias UIViewControllerType = SLCalendarViewController
+}
+
+extension Array {
+  subscript(safe index: Int) -> Element? {
+    if indices.contains(index) {
+      return self[index]
+    } else {
+      return nil
+    }
+  }
 }
